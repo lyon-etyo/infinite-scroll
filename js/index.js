ae3786bf92249d31b00c0a71079d196ad3bc35ac
Lyon Etyo <mulyonoeko.prastyo@gmail.com>
2021-06-24 13:28:03 +0700

    change scroll event listener to Intersection Observer

--- js/index.js
+++ js/index.js
@@ -55,67 +55,81 @@ customElements.define(
   }
 );
 
-const blogsContainer = document.querySelector(".blogs");
-const searchForm = document.forms[0];
-let uri = "http://localhost:3000/posts?_sort=title&_page=1&_limit=3";
-let next;
-let term;
+window.addEventListener("DOMContentLoaded", _ => {
+  // Access DOM elements
+  const blogsContainer = document.querySelector(".blogs");
+  const spinner = document.querySelector(".spinner-1");
+  const searchForm = document.forms[0];
+  let next = "";
+  let term = "";
 
-async function renderPosts(uri, term = null) {
-  uri = term ? `${uri}&q=${term}` : uri;
-  console.log(next);
-  if (!uri) return;
-  blogsContainer.nextElementSibling.classList.toggle("is-loading", true);
-  const res = await fetch(uri);
-  const posts = await res.json();
-  const span = document.createElement("span");
-  const fragment = document.createDocumentFragment();
-  posts.forEach(post => {
-    const blogPost = document.createElement("blog-post");
-    const title = span.cloneNode();
-    const likes = span.cloneNode();
-    const body = span.cloneNode();
-    title.textContent = post.title;
-    likes.textContent = post.likes;
-    body.textContent = post.body.slice(0, 150) + "...";
-    title.slot = "title";
-    likes.slot = "likes";
-    body.slot = "body";
-    blogPost.dataset.id = post.id;
-    blogPost.appendChild(title);
-    blogPost.appendChild(likes);
-    blogPost.appendChild(body);
-    fragment.appendChild(blogPost);
+  // First time data load render
+  renderPosts();
+
+  // create Intersection Observer options
+  const options = {
+    root: null,
+    rootMargins: "0px",
+    threshold: 0,
+  };
+  // observe the element if intersect and next link is exist then fetch data and render to the DOM
+  const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting && next) renderPosts(next);
+  }, options);
+  observer.observe(spinner.parentElement);
+
+  searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (this.term.value.trim() === term) return;
+    term = this.term.value.trim();
+    next = `http://localhost:3000/posts?_sort=title&_page=1&_limit=3&q=${term}`;
+    blogsContainer.innerHTML = "";
+    observer.observe(spinner.parentElement);
   });
-  blogsContainer.appendChild(fragment);
-  if (res.headers.get("link").indexOf("next") > -1) {
-    const linksArray = res.headers.get("link").split(",");
+
+  async function renderPosts(uri = "http://localhost:3000/posts?_sort=title&_page=1&_limit=3") {
+    // add class "is-loading"
+    spinner.classList.toggle("is-loading", true);
+    // fetch data
+    const res = await fetch(uri);
+    const posts = await res.json();
+    // get next link from headers
+    next = getNextLink(res.headers);
+    // unobserver infinite scroll if next link not exist
+    if (!next) observer.unobserve(spinner.parentElement);
+    // Rendering to the DOM
+    render(posts, blogsContainer);
+    // remove class "is-loading"
+    spinner.classList.toggle("is-loading", false);
+  }
+
+  function render(data, containerElement) {
+    const span = document.createElement("span");
+    const fragment = document.createDocumentFragment();
+    data.forEach(post => {
+      const blogPost = document.createElement("blog-post");
+      const title = span.cloneNode();
+      const likes = span.cloneNode();
+      const body = span.cloneNode();
+      title.textContent = post.title;
+      likes.textContent = post.likes;
+      body.textContent = post.body.slice(0, 150);
+      title.slot = "title";
+      likes.slot = "likes";
+      body.slot = "body";
+      blogPost.dataset.id = post.id;
+      blogPost.appendChild(title);
+      blogPost.appendChild(likes);
+      blogPost.appendChild(body);
+      fragment.appendChild(blogPost);
+    });
+    containerElement.appendChild(fragment);
+  }
+
+  function getNextLink(headers) {
+    if (headers.get("link").indexOf("next") < 0) return "";
+    const linksArray = headers.get("link").split(",");
     const nextLinkArray = linksArray[linksArray.length - 2].split(";");
-    const regex = /[<>]/g;
-    next = nextLinkArray[0].replace(regex, "");
-  } else {
-    next = false;
-    blogsContainer.removeEventListener("scroll", handleScroll);
+    return nextLinkArray[0].replace(new RegExp(/[<>]/g), "");
   }
-  blogsContainer.nextElementSibling.classList.toggle("is-loading", false);
-}
-
-function handleScroll() {
-  let triggerHeight = this.scrollTop + this.offsetHeight;
-  if (triggerHeight >= this.scrollHeight) {
-    renderPosts(next);
-    console.log("fetching");
-  }
-}
-
-searchForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  if (this.term.value.trim() === term) return;
-  blogsContainer.innerHTML = "";
-  blogsContainer.addEventListener("scroll", handleScroll);
-  renderPosts(uri, this.term.value.trim());
-  term = this.term.value.trim();
 });
-window.addEventListener("DOMContentLoaded", _ => renderPosts(uri));
-
-blogsContainer.addEventListener("scroll", handleScroll);
